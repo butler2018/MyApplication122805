daa83d7cf6dbab7450b26c6d746db92b0a98f2ff
calvin <butler2018@hotmail.com>
2017-12-28 16:08:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/myapplication122805/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.student.myapplication122805;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click1(View v)
+    {
+        Uri uri = Uri.parse("http://tw.yahoo.com");
+        Intent it = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(it);
+    }
+    public void click2(View v)
+    {
+        Uri uri = Uri.parse("geo:0,0?q=巨匠電腦三重認證");
+        Intent it = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(it);
+
+    }
+    public void click3(View v)
+    {
+        Uri uri = Uri.parse("tel:02 2972 0388");
+        Intent it = new Intent(Intent.ACTION_DIAL, uri);
+        startActivity(it);
+    }
+
+}
